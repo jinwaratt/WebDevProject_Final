4d6d1586b60c537ccf3f838939064e378d7b7511
takaboutfruit <takaboutfruit@gmail.com>
2026-04-07 18:49:16 +0700

    frontend server

---
+++ sec2_gr01_fe_src/app.js
@@ -0,0 +1,42 @@
+// Import libraries
+const express = require('express')
+const path = require('path')
+
+// Config dotenv
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+
+// Router object
+const router = express.Router()
+app.use(router)
+// app.use('/', express.static(path.join(__dirname, 'public')))
+
+// Routing
+router.get('/', (req, res) =>{
+    res.sendFile(path.join(`${__dirname}/home-page.html`))
+})
+
+router.get('/login', (req, res) =>{
+    res.sendFile(path.join(`${__dirname}/login.html`))
+})
+
+router.get('/search', (req, res) =>{
+    res.sendFile(path.join(`${__dirname}/search.html`))
+})
+
+router.get('/team', (req, res) =>{
+    res.sendFile(path.join(`${__dirname}/team-page.html`))
+})
+
+// Handle other unspecific paths
+router.use((req, res, next)=>{
+    console.log("404: Invalid accessed")
+    res.status(404).send("Invalid path")
+})
+
+// Listen
+app.listen(process.env.PORT, function(){
+    console.log(`Server listening at Port ${process.env.PORT}`)
+})
